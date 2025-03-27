e9c03b7a237a26b1df0c1c1ce9f3fc4bd81ee477
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2025-03-27 20:37:47 +0900

    Create L1L2.h

---
+++ CSE302/source/model/regularization/L1L2.h
@@ -0,0 +1,36 @@
+//
+//  L1L2.h
+//  CSE302
+//
+//  Created by Sora Sugiyama on 3/26/25.
+//
+
+#ifndef L1L2_h
+#define L1L2_h
+
+#include <cmath>
+#include "matrix.h"
+
+namespace model{
+
+namespace regularization{
+
+double L1(double loss,linAlge::mat &w,const double lambda=1){
+    for(int i=0;i<w.n;i++){
+        for(int j=0;j<w.m;j++)loss+=abs(w(i,j));
+    }
+    return loss;
+}
+
+double L2(double loss,linAlge::mat &w,const double lambda=1){
+    for(int i=0;i<w.n;i++){
+        for(int j=0;j<w.m;j++)loss+=w(i,j)*w(i,j);
+    }
+    return loss;
+}
+
+}
+
+}
+
+#endif /* L1L2_h */
