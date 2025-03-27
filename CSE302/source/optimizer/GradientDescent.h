d70684436f7b4f4a0fca70e129de551da4e89292
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2025-03-27 20:35:06 +0900

    Create GradientDescent.h

---
+++ CSE302/source/optimizer/GradientDescent.h
@@ -0,0 +1,80 @@
+//
+//  GradientDescent.h
+//  CSE302
+//
+//  Created by Sora Sugiyama on 3/26/25.
+//
+
+#ifndef GradientDescent_h
+#define GradientDescent_h
+
+#include "../linAlge/matrix.h"
+#include <vector>
+#include <cstddef>
+#include <iostream>
+#include <stack>
+
+namespace optimizer{
+
+using u32=uint_fast32_t;
+
+template<
+class F,
+class DF,
+class Act,
+class DAct
+>void gradientDescent(
+                      F loss,
+                      DF dloss,
+                      Act actf,
+                      DAct dactf,
+                      std::vector<linAlge::mat>&coef,
+                      linAlge::mat &X,
+                      linAlge::mat &Y,
+                      const double theta,
+                      u32 gn=10000,const u32 &printCur=1<<30){
+    
+    u32 i=0,ii=0,jj=0;
+    
+    for(i=1;i<=gn;i++){
+        std::stack<linAlge::mat>log;
+        linAlge::mat x=X;
+        
+        // Forward Prop
+        for(linAlge::mat &w:coef){
+            log.push(x);
+            x=x*w;
+            for(ii=0;ii<x.n;ii++){
+                for(jj=0;jj<x.m;jj++)x(ii,jj)=actf(x(ii,jj));
+            }
+        }
+
+        x=dloss(x,Y);
+        for(ii=0;ii<x.n;ii++){
+            for(jj=0;jj<x.m;jj++){
+                x(ii,jj)=dactf(x(ii,jj));
+            }
+        }
+        
+        if((i+1)%printCur==0){
+            std::cout<<"Before the "<<i<<"th step, Current loss: "<<loss(x,Y)<<std::endl;
+        }
+        
+        // Backward Prop
+        for(auto it=coef.rbegin();it!=coef.rend();it++){
+            linAlge::mat Z=log.top().T();log.pop();
+            linAlge::mat &w=*it;
+            x=Z*x;
+            for(ii=0;ii<x.n;ii++){
+                for(jj=0;jj<x.m;jj++){
+                    x(ii,jj)=dactf(x(ii,jj));
+                    w(ii,jj)-=theta*x(ii,jj);
+                }
+            }
+        }
+    }
+}
+
+}
+
+#endif /* GradientDescent_h */
