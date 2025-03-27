7fb1a1cd9be12b26e5c043e43b1a7a19b7e2d6d9
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2025-03-27 22:19:33 +0900

    Update logisticAct.h

--- CSE302/source/model/actFunction/logisticAct.h
+++ CSE302/source/model/actFunction/logisticAct.h
@@ -15,10 +15,14 @@ namespace model{
 namespace actFunction{
 
 double logistic(double x){
+    x=50>x?x:50;
+    x=-50<x?x:50;
     return 1/(1+exp(-x));
 }
 
 double dlogistic(double x){
+    x=50>x?x:50;
+    x=-50<x?x:50;
     const double ex=exp(-x);
     return ex/(ex*ex+ex+ex+1);
 }
