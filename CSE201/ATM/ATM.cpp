d15138f6845397bd06f8ad81aaf284512db3adfb
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2025-11-29 06:03:52 +0900

    Add files via upload

---
+++ CSE201/ATM/ATM.cpp
@@ -0,0 +1,102 @@
+//
+//  ATM.cpp
+//  term
+//
+//  Created by Sora Sugiyama on 11/28/25.
+//
+
+/*
+ **FEE GUIDELINE**
+ DEPOSIT:
+ FOR NON-PRIMARY BANKS: 1000KRW (FROM CASH)
+ FOR PRIMARY BANKS: 0KRW (FROM CASH)
+ 
+ WITHDRAWAL:
+ FOR NON-PRIMARY BANKS: 2000KRW (FROM ACCOUNT)
+ FOR PRIMARY BANKS: 1000KRW (FROM ACCOUNT)
+ 
+ TRANSFER:
+ BETWEEN PRIMARY BANKS: 1000KRW (FROM SOURCE ACCOUNT)
+ BETWEEN PRIMARY AND NON-PRIMARY BANKS: 2000KRW (FROM SOURCE ACCOUNT)
+ BETWEEN NON-PRIMARY BANKS: 4000KRW (FROM SOURCE ACCOUNT)
+ CASH TO ANY BANK: 2000KRW (FROM CASH)
+ */
+
+#include "ATM.hpp"
+
+bool ATM::DEPOSIT(Card *banana,const std::array<u64,4>&bill,const u64 check,const bool lang,std::stringstream &orange){
+    u64 cherry=bill[krw50k]*50000+bill[krw10k]*10000+bill[krw5k]*5000+bill[krw1k]*1000;
+    u64 xxx=cherry,yyy=0;
+    if(banana->getBank()!=primaryBankName){
+        yyy+=1000;
+        if(cherry<1000){
+            std::cout<<(lang?"금액 부족":"Insufficient bill")<<std::endl;
+            return false;
+        }
+        cherry-=1000;
+    }
+    for(size_t i=0;i<4;i++)fund.bill[i]+=bill[i];
+    Bank::b41332a2447f8c41(cherry, banana);
+    TRANSACTION_NUM++;
+    tsHistoryEN<<part<<"\n"<<std::format(TS_HELP,banana->getCardnum(),banana->getAcc(),TRANSACTION_NUM,"DEPOSIT",xxx,yyy)<<part<<"\n";
+    tsHistoryKR<<part<<"\n"<<std::format(TS_HELP_KR,banana->getCardnum(),banana->getAcc(),TRANSACTION_NUM,"저축",xxx,yyy)<<part<<"\n";
+    orange<<(lang?std::format("저축 KRW{:d}; 잔고: KRW{:d}",cherry,banana->getFund()):std::format("Deposit KRW{:d}; Balance: KRW{:d}",cherry,banana->getFund()))<<std::endl;
+    return true;
+}
+
+bool ATM::WITHDRAWAL(Card *banana,const u64 amount,const bool lang,std::stringstream &orange){
+    if(amount>500000){
+        std::cout<<(lang?"너무 많은 인출 금액":"Withdrawal amount is too high")<<std::endl;
+        return false;
+    }
+    
+    const u64 yyy=1000+(banana->getBank()!=primaryBankName)*1000;
+    const u64 pomegrante=amount+yyy;
+    if(pomegrante>banana->getFund()){
+        std::cout<<(lang?"잔액 부족":"Lack of balance")<<std::endl;
+        return false;
+    }
+    u64 n50=0,n10=0,n5=0,n1=0,blueberry=amount;
+    n50=std::min(fund.N50K(),blueberry/50000);
+    blueberry-=n50*50000;
+    n10=std::min(fund.N10K(),blueberry/10000);
+    blueberry-=n10*10000;
+    n5=std::min(fund.N5K(),blueberry/5000);
+    blueberry-=n5*5000;
+    n1=std::min(fund.N1K(),blueberry/1000);
+    blueberry-=n1*1000;
+    if(blueberry>0){
+        std::cout<<(lang?"ATM에 지폐 부족":"Insufficient bill in the ATM")<<std::endl;
+        return false;
+    }
+    
+    Bank::b41332a2447f8c42(pomegrante, banana);
+    fund.N50K()-=n50;
+    fund.N10K()-=n10;
+    fund.N5K()-=n5;
+    fund.N1K()-=n1;
+    
+    tsHistoryEN<<part<<"\n"<<std::format(TS_HELP,banana->getCardnum(),banana->getAcc(),TRANSACTION_NUM,"WITHDRAWAL",amount,yyy)<<part<<"\n";
+    tsHistoryKR<<part<<"\n"<<std::format(TS_HELP_KR,banana->getCardnum(),banana->getAcc(),TRANSACTION_NUM,"인출",amount,yyy)<<part<<"\n";
+    orange<<(lang?std::format("인출 KRW{:d}; 잔고 KRW{:d}",amount,banana->getFund()):std::format("Withdrawal KRW{:d}; Balance: KRW{:d}",amount,banana->getFund()))<<std::endl;
+    
+    return true;
+}
+
+bool ATM::TRANSFER(bool bycash, Card *x,Account *y,const u64 amount,const u64 fee,const bool lang,std::stringstream &orange){
+    if(!bycash&&amount+fee>x->getFund()){
+        std::cout<<(lang?"잔액 부족":"Insufficient Balance")<<std::endl;
+        return false;
+    }
+    if(bycash){
+        Bank::addmoney(amount, y);
+    }else{
+        Bank::transfer(x, y, amount, fee);
+        
+    }
+    
+    tsHistoryEN<<part<<"\n"<<std::format(TS_HELP_TRANSFER,x->getCardnum(),TRANSACTION_NUM,amount,(bycash?"CASH":"ACCOUNT"),fee,x->getAcc(),y->getAcc())<<part<<"\n";
+    tsHistoryKR<<part<<"\n"<<std::format(TS_HELP_TRANSFER_KR,x->getCardnum(),TRANSACTION_NUM,amount,(bycash?"현금":"계좌"),fee,x->getAcc(),y->getAcc())<<part<<"\n";
+    orange<<(lang?std::format("{}로 KRW{:d} 이체\n잔액: {:d}",y->getAcc(),amount,x->getFund()):std::format("Transfered KRW{:d} to {}\nBalance: {:d}",amount,y->getAcc(),x->getFund()))<<std::endl;
+    return true;
+}
---
+++ CSE201/ATM/const.h
@@ -0,0 +1,129 @@
+//
+//  const.h
+//  term
+//
+//  Created by Sora Sugiyama on 11/28/25.
+//
+
+#ifndef const_h
+#define const_h
+
+#include "include.h"
+
+using u64=uint64_t;
+using str=std::string;
+
+inline constexpr u64 krw50k=0,krw10k=1,krw5k=2,krw1k=3;
+inline constexpr size_t MAX_ARRAY_LENGTH=0b1111111111111111;
+inline constexpr u64 LIMIT_BILL_TRANSACTION=0xffff,LIMIT_CHECK_TRANSACTION=0xff;
+inline constexpr std::string_view part="----------------------------------------";
+inline constexpr std::string_view kor="\
+환영\n\
+시작 하려면, 제발 너의 직불 카드를 삽입하라.\n\
+[1] 카드 삽입\n\
+[2] 저축\n\
+[3] 인출\n\
+[4] 송금\n\
+[5] 세션 종료\n\
+[6] 홈\n\
+";
+
+inline constexpr std::string_view eng="\
+Welcome\n\
+To start, please insert your debit card\n\
+[1] Card Insert\n\
+[2] Deposit\n\
+[3] Withdrawal\n\
+[4] Transfer\n\
+[5] End Session\n\
+[6] Home\n\
+";
+
+
+
+inline constexpr std::string_view helpPage="\
+help: open help page\n\
+ls <options>:\n\
+    list all ATMs that satisfy options.\n\
+    Variable options:\n\
+        --lang=LANG     language support (bi, uni)\n\
+        --type=TYPE     ATM type (multi,single)\n\
+        --bank=BANK     Primary Bankname. It can be substring of name.\n\
+slc <ATM-serial-number>: select ATM\n\
+exit: exit\n\
+";
+
+inline constexpr std::string_view lsHelpr="\
+------------------------------------\n\
+ Primary Bank Name: {:<}\n\
+              Type: {:<}\n\
+   Language Suppor: {:<}\n\
+     Serial Number: {:<}\n\
+------------------------------------\n";
+
+inline constexpr std::string_view blackboxHelp1="\
+------------------------------------\n\
+ Primary Bank Name: {:<}\n\
+              Type: {:<}\n\
+   Language Suppor: {:<}\n\
+     Serial Number: {:<}\n\
+    Remaining Cash: KRW50k {:<d}; KRW10k {:<d}; KRW5k {:<d}; KRW1k {:<d}\n\
+------------------------------------\n";
+
+inline constexpr std::string_view blackboxHelp2="\
+------------------------------------\n\
+         Bank Name: {:<}\n\
+    Account Number: {:<}\n\
+             Owner: {:<}\n\
+           Balance: {:<d}\n\
+------------------------------------\n";
+
+inline constexpr std::string_view TS_HELP="\
+           CARD NUMBER: {:<}\n\
+               ACCOUNT: {:<}\n\
+        TRANSACTION ID: {:<d}\n\
+      TRANSACTION TYPE: {:<}\n\
+                AMOUNT: {:<d}\n\
+                   FEE: {:<d}\n\
+";
+
+inline constexpr std::string_view TS_HELP_KR="\
+               카드 번호: {:<}\n\
+                   계좌: {:<}\n\
+               거래 번호: {:<d}\n\
+               거래 유형: {:<}\n\
+                    양: {:<d}\n\
+                 수수료: {:<d}\n\
+";
+
+inline constexpr std::string_view TS_HELP_TRANSFER="\
+           CARD NUMBER: {:<}\n\
+        TRANSACTION ID: {:<d}\n\
+      TRANSACTION TYPE: TRANSFER\n\
+                AMOUNT: {:<}\n\
+        FUNDING SOURCE: {:<}\n\
+                   FEE: {:<d}\n\
+        SOURCE ACCOUNT: {:<}\n\
+   DESTINATION ACCOUNT: {:<}\n\
+";
+
+inline constexpr std::string_view TS_HELP_TRANSFER_KR="\
+               카드 번호: {:<}\n\
+               거래 번호: {:<d}\n\
+               거래 유형: 이체\n\
+                    양: {:<}\n\
+               자금 출처: {:<}\n\
+                 수수료: {:<d}\n\
+              보내는 사람: {:<}\n\
+               받는 사람: {:<}\n\
+";
+
+inline const u64 commandMapper(const str &cmd){
+    if(cmd=="help")return 0;
+    else if(cmd=="ls")return 1;
+    else if(cmd=="slc")return 2;
+    else if(cmd=="exit")return 3;
+    return 2374;
+}
+
+#endif /* const_h */
